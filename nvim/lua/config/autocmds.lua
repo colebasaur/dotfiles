bed1c4113b43174d34319ea03eb8d419e956a19b
cole.troutman <cole.troutman@youversion.com>
2024-03-28 15:35:06 -0500

    disable kotlin autoformat and tf

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -2,9 +2,17 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
----- Disable autoformat for lua files
+---- Disable autoformat for kotlin files
 vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = { "kotlin" },
+  callback = function()
+    vim.b.autoformat = false
+  end,
+})
+
+---- Disable autoformat for TF files
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { ".tf" },
   callback = function()
     vim.b.autoformat = false
   end,
