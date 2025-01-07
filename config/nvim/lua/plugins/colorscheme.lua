ccaaa4afcc285e5da6b4a3e473b0807d40fdf70e
cole.troutman <cole.troutman@youversion.com>
2025-01-06 18:33:37 -0600

    share history, colorscheme

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,21 +1,14 @@
--- return {
---   "rebelot/kanagawa.nvim",
---   lazy = false,
---   priority = 1000,
---   config = function()
---     vim.cmd([[colorscheme kanagawa-wave]]) -- dark
---     -- vim.cmd([[colorscheme kanagawa-dragon]]) -- darker
---     -- vim.cmd([[colorscheme kanagawa-lotus]]) -- light
---   end,
--- }
 return {
   "catppuccin/nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.cmd([[colorscheme catppuccin-mocha]]) -- darkest
-    -- vim.cmd([[colorscheme catppuccin-macchiato]]) -- darker
-    -- vim.cmd([[colorscheme catppuccin-frappe]]) -- dark
-    -- vim.cmd([[colorscheme catppuccin-latte]]) -- light
+  opts = {
+    flavour = "mocha",
+    transparent_background = true,
+    styles = {
+    }
+  },
+  config = function(_, opts)
+    require("catppuccin").setup(opts)
+    vim.cmd("colorscheme catppuccin")
   end,
 }
+-- hello world
