8a0943f95286d60a2acf427aa2eee6861b7f3039
cole.troutman <cole.troutman@youversion.com>
2025-02-15 21:46:35 -0600

    updates for colorschemes

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -11,10 +11,9 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
-    -- { import = "lazyvim.plugins.extras.ui.mini-starter" },
-    -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.editor.harpoon2" },
+    { import = "lazyvim.plugins.extras.editor.telescope" },
+    { import = "lazyvim.plugins.extras.formatting.black" },
     -- import/override with your plugins
     { import = "plugins" },
   },
@@ -27,7 +26,6 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  -- install = { colorscheme = { "tokyonight", "habamax" } },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,41 @@
 return {
-  "catppuccin/nvim",
-  opts = {
-    flavour = "mocha",
-    transparent_background = true,
-    styles = {
-    }
+  {
+    "catppuccin/nvim",
+    opts = {
+      flavour = "mocha",
+      transparent_background = true,
+      styles = {},
+    },
   },
-  config = function(_, opts)
-    require("catppuccin").setup(opts)
-    vim.cmd("colorscheme catppuccin")
-  end,
+  { "ellisonleao/gruvbox.nvim" },
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "moon" },
+  },
+  {
+    "EdenEast/nightfox.nvim",
+  },
+  {
+    "shaunsingh/moonlight.nvim",
+  },
+  {
+    "shaunsingh/nord.nvim",
+  },
+  -- Configure LazyVim to load colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+      -- colorscheme = "gruvbox",
+      -- colorscheme = "tokyonight",
+      -- colorscheme = "duskfox",
+      -- colorscheme = "nightfox",
+      -- colorscheme = "terafox",
+      -- colorscheme = "nordfox",
+      -- colorscheme = "carbonfox",
+      -- colorscheme = "moonlight",
+      -- colorscheme = "nord",
+    },
+  },
 }
--- hello world
