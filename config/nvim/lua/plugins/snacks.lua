dc5e8af73b3392f4bd51a8e0d61f71797e54df8f
cole.troutman <cole.troutman@youversion.com>
2025-02-16 13:23:47 -0600

    show hidden and ignored by default

---
+++ config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,17 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      sources = {
+        explorer = {
+          hidden = true, -- Show hidden files
+          ignored = true, -- Show git ignored files
+        },
+        files = { -- Also configure the files source
+          hidden = true,
+          ignored = true,
+        },
+      },
+    },
+  },
+}
