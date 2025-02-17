cc01feeb9fc451c60b98b3ffe21609b1675aad96
cole.troutman <cole.troutman@youversion.com>
2025-02-16 18:25:47 -0600

    make bufferline sloped

---
+++ config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,10 @@
+return {
+  "akinsho/bufferline.nvim",
+  opts = {
+    options = {
+      -- separator_style = "slant" | "slope" | "thick" | "thin" | { 'any', 'any' },
+      separator_style = "slant",
+      themable = true,
+    },
+  },
+}
--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -15,7 +15,7 @@ return {
   },
   {
     "EdenEast/nightfox.nvim",
-    opts = { options = { transparent = true } },
+    -- opts = { options = { transparent = true } },
   },
   {
     "shaunsingh/moonlight.nvim",
