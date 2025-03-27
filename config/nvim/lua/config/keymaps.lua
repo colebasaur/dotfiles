84027e8c90c105444447894ae0eebaa7277bb3d5
cole.troutman <cole.troutman@youversion.com>
2025-03-27 11:56:12 -0500

    less disorienting half-page jumps

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -2,9 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("i", "jk", "<Esc>", { silent = true })
--- vim.keymap.set("n", "<C-j>", "4jzz", { silent = true })
--- vim.keymap.set("n", "<C-k>", "4kzz", { silent = true })
--- vim.keymap.set("n", "G", "Gzz", { silent = true })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { silent = true })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { silent = true })
 vim.keymap.set("n", "<leader>mt", ":RenderMarkdown toggle<CR>", { silent = true })
 
 -- Parrot AI implementations
