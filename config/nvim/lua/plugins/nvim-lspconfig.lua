ba332c0127869c0090225152b7961f42431d7304
cole.troutman <cole.troutman@youversion.com>
2025-05-04 21:11:45 -0500

    tailwindcss ...

--- config/nvim/lua/plugins/nvim-lspconfig.lua
+++ config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -5,17 +5,7 @@ return {
       -- pyright will be automatically installed with mason and loaded with lspconfig
       pyright = {},
       dartls = {},
-      tailwindcss = {
-        filetypes = { "html", "javascript", "typescript" },
-        settings = {
-          experimental = {
-            classRegex = {
-              'class="([^"]*)',
-              'className="([^"]*)',
-              'class: "([^"]*)',
-            },
-          },
-        },
+      tailwindcss = {},
       },
     },
   },
