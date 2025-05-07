40f1e68d534458a4d68b6c3e01121526add2cf87
cole.troutman <cole.troutman@youversion.com>
2025-05-06 21:24:39 -0500

    pin mason

---
+++ config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,8 @@
+return {
+  "mason-org/mason-lspconfig.nvim",
+  version = "v1.32.0", -- Pin to the last version before the breaking change
+  dependencies = {
+    "mason-org/mason.nvim",
+    "neovim/nvim-lspconfig",
+  },
+}
